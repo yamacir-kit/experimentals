4a670286eddb1c4498a650eab2d7c00a7623d810
YamaCIR-KIT <httperror@404-notfound.jp>
2017-03-04 22:32:16 +0900

    Add color class

--- include/meevax/trial/ansi_escape_seqence.hpp
+++ include/meevax/trial/ansi_escape_seqence.hpp
@@ -4,60 +4,50 @@
 
 #include <ostream>
 #include <string>
-#include <array>
 
 #include <cstdint>
+
+#include <meevax/trial/constexpr_string.hpp>
 
 
 namespace meevax {
 
 
-// class ansi_escape_seqence
-// {
-//   static constexpr int8_t escape {0x1b}; // 0d27, 033
-//   constexpr char*
-//
-// public:
-//   constexpr explicit ansi_escape_seqence()
-// };
-
-class text_attribute
+class ansi_escape_seqence
+  : public meevax::constexpr_string<char>
 {
 public:
-  static constexpr int_fast8_t reset      {0};
-  static constexpr int_fast8_t bold       {1};
-  static constexpr int_fast8_t underscore {4};
-  static constexpr int_fast8_t blink      {5};
-  static constexpr int_fast8_t reverse    {7};
-  static constexpr int_fast8_t consealed  {8};
-};
+  static constexpr char escape {0x1b};
 
+  class text_attribute
+    : public meevax::constexpr_string<char>
+  {
+  public:
+    static constexpr meevax::constexpr_string<char> reset      {"0"};
+    static constexpr meevax::constexpr_string<char> bold       {"1"};
+    static constexpr meevax::constexpr_string<char> underscore {"4"};
+    static constexpr meevax::constexpr_string<char> blink      {"5"};
+    static constexpr meevax::constexpr_string<char> reverse    {"7"};
+    static constexpr meevax::constexpr_string<char> consealed  {"8"};
+  };
 
-class foreground_color
-{
-public:
-  static constexpr int_fast8_t black   {30};
-  static constexpr int_fast8_t red     {31};
-  static constexpr int_fast8_t green   {32};
-  static constexpr int_fast8_t yellow  {33};
-  static constexpr int_fast8_t blue    {34};
-  static constexpr int_fast8_t magenta {35};
-  static constexpr int_fast8_t cyan    {36};
-  static constexpr int_fast8_t white   {37};
-};
+  class foreground_color
+    : public meevax::constexpr_string<char>
+  {
+  public:
+    static constexpr meevax::constexpr_string<char> black   {"30"};
+    static constexpr meevax::constexpr_string<char> red     {"31"};
+    static constexpr meevax::constexpr_string<char> green   {"32"};
+    static constexpr meevax::constexpr_string<char> yellow  {"33"};
+    static constexpr meevax::constexpr_string<char> blue    {"34"};
+    static constexpr meevax::constexpr_string<char> magenta {"35"};
+    static constexpr meevax::constexpr_string<char> cyan    {"36"};
+    static constexpr meevax::constexpr_string<char> white   {"37"};
+  };
 
-
-class background_color
-{
-public:
-  static constexpr int_fast8_t black   {40};
-  static constexpr int_fast8_t red     {41};
-  static constexpr int_fast8_t green   {42};
-  static constexpr int_fast8_t yellow  {43};
-  static constexpr int_fast8_t blue    {44};
-  static constexpr int_fast8_t magenta {45};
-  static constexpr int_fast8_t cyan    {46};
-  static constexpr int_fast8_t white   {47};
+  class background_color
+    : public meevax::ansi_escape_seqence::foreground_color
+  {};
 };
 
 
