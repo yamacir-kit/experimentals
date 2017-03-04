053cc4c2bd9830a997f37f63efdc34876dcbaf65
YamaCIR-KIT <httperror@404-notfound.jp>
2017-03-04 16:53:24 +0900

    Add arguments parser (from utilib)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
-#include <utility>
 #include <vector>
 
-#include <cerrno>
-#include <cstring>
+#include <meevax/trial/arguments_parser.hpp>
 
 
 int main(int argc, char** argv)
 {
+  std::vector<std::string> argv_ {
+    "./utilib",
+    "-abc",
+    "--help",
+    "-n42",
+    "-n", "42",
+    "--long1=arg1",
+    "--long2", "arg2",
+    "-",
+    "-p",
+    "--piyo"
+  };
+
+  meevax::arguments_parser parser {std::move(argv_)};
 
   return 0;
 }
