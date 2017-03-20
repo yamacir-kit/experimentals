d1295739b62b6fe1dadda2f63e5004a622ce7703
YamaCIR-KIT <httperror@404-notfound.jp>
2017-03-20 18:02:17 +0900

    Remove trial headers

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,8 @@
 #include <string>
 #include <vector>
 
-// #include <meevax/trial/arguments_parser.hpp>
-// #include <meevax/trial/ansi_escape_seqence.hpp>
-
 
 int main(int argc, char** argv)
 {
-  std::vector<std::string> argv_ {
-    "./utilib",
-    "-abc",
-    "--help",
-    "-n42",
-    "-n", "42",
-    "--long1=arg1",
-    "--long2", "arg2",
-    "-",
-    "-p",
-    "--piyo"
-  };
-
-  // meevax::arguments_parser parser {std::move(argv_)};
-
   return 0;
 }
