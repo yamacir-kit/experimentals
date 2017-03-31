d7fca596b9dcd0e213204685ff28b4546976009d
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-01 04:11:53 +0900

    Remove two arguments from constructor
    std::cin と std::cout についていろいろ勘違いしていた
    これを解決しようとすると盛大にこじらせる気がしたため保留へ
    
    前に作った vectorized_iostream が使えそうな気がしてきた（危険な匂い）

--- include/meevax/unix/shell.hpp
+++ include/meevax/unix/shell.hpp
@@ -24,41 +24,37 @@ private: // internal data
   const std::vector<std::basic_string<char_type>> argv_;
   const             std::basic_string<char_type>  name_; // program called this class
 
-  const std::basic_istream<char_type> cin_;
-  const std::basic_ostream<char_type> cout_;
-
 public:
-  explicit shell(int argc, char** argv,
-                 const std::basic_istream<char_type>& cin  = std::cin,
-                 const std::basic_ostream<char_type>& cout = std::cout)
+  explicit shell(int argc, char** argv)
     : argv_ {argv, argv + argc},
-      name_ {argv_[0].substr(argv_[0].find_last_of('/') + 1)},
-      cin_  {cin},
-      cout_ {cout}
+      name_ {argv_[0].substr(argv_[0].find_last_of('/') + 1)}
   {
-    static_assert(std::basic_string<char_type>::npos == 1,
+    static_assert(std::basic_string<char_type>::npos == -1,
                   "the premise has collapsed. report this to the developer.");
+
+    for (const auto& v : help(argv_))
+      for (const auto& s : v) std::cout << s << (&s != &v.back() ? ' ' : '\n');
   };
 
-  auto write(const std::basic_string<char_type>& word) const
-    -> decltype(cout_)
-  {
-    return cout_ << word << std::endl;
-  }
-
-  auto write(const std::vector<std::basic_string<char_type>>& line) const
-    -> decltype(cout_)
-  {
-    for (const auto& s : line) cout_ << s << (&s != &line.back() ? ' ' : '\n');
-    return cout_;
-  }
-
-  auto write(const std::vector<std::vector<std::basic_string<char_type>>>& text) const
-    -> decltype(cout_)
-  {
-    for (const auto& v : text) write(v);
-    return cout_;
-  }
+  // auto& write(const std::basic_string<char_type>& word) const
+  //   -> decltype(std::cout)
+  // {
+  //   return &(std::cout << word << std::endl);
+  // }
+  //
+  // auto& write(const std::vector<std::basic_string<char_type>>& line) const
+  //   -> decltype(std::cout)
+  // {
+  //   for (const auto& s : line) std::cout << s << (&s != &line.back() ? ' ' : '\n');
+  //   return &std::cout;
+  // }
+  //
+  // auto& write(const std::vector<std::vector<std::basic_string<char_type>>>& text) const
+  //   -> decltype(std::cout)
+  // {
+  //   for (const auto& v : text) write(v);
+  //   return &std::cout;
+  // }
 
 
   static auto version(const std::vector<std::basic_string<char_type>>&)
