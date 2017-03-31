f4cee41854a47f2f73e8074324a66d40d6ba606b
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-01 03:05:57 +0900

    Update input and output to be changeable

--- include/meevax/unix/shell.hpp
+++ include/meevax/unix/shell.hpp
@@ -24,10 +24,17 @@ private: // internal data
   const std::vector<std::basic_string<char_type>> argv_;
   const             std::basic_string<char_type>  name_; // program called this class
 
+  const std::basic_istream<char_type> cin_;
+  const std::basic_ostream<char_type> cout_;
+
 public:
-  explicit shell(int argc, char** argv)
+  explicit shell(int argc, char** argv,
+                 const std::basic_istream<char_type>& cin  = std::cin,
+                 const std::basic_ostream<char_type>& cout = std::cout)
     : argv_ {argv, argv + argc},
-      name_ {argv_[0].substr(argv_[0].find_last_of('/') + 1)}
+      name_ {argv_[0].substr(argv_[0].find_last_of('/') + 1)},
+      cin_  {cin},
+      cout_ {cout}
   {
     static_assert(std::basic_string<char_type>::npos == 1,
                   "the premise has collapsed. report this to the developer.");
