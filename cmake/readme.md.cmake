4bc5070b20f6006ccdce9b8ecc285b58007917af
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-03 06:32:51 +0900

    Update version

---
+++ cmake/readme.md.cmake
@@ -0,0 +1,8 @@
+### ${PROJECT_NAME}
+
+version ${PROJECT_VERSION} in development
+
+
+### dependency
+
+- C++14 standard library
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ### meevax
 
-version 0.1.0 in development
+version 0.1.1 in development
 
 
 ### dependency
