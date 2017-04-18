723425bab68141d412695ea3c70c35b08a5b1f0c
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-18 20:56:48 +0900

    Update debug code
    疲れた
    
    こうして整理してみると結構キーコードがバラバラだと分かる
    アルファベットの小文字大文字の変換がビットひとつ書き換えるだけというのは
    有名な上に有用だから俺でも知ってるくらい
    
    そういうあれこれがここにはたくさん眠っているはず

--- include/meevax/preproc/key_bind.cpp
+++ include/meevax/preproc/key_bind.cpp
@@ -1,512 +1,512 @@
 case   0:
-  std::cout << "[debug] key code:   0" << std::endl;
+  std::cout << "[debug] Ctrl + 2" << std::endl;
   break;
 
 case   1:
-  std::cout << "[debug] key code:   1" << std::endl;
+  std::cout << "[debug] Ctrl + A" << std::endl;
   break;
 
 case   2:
-  std::cout << "[debug] key code:   2" << std::endl;
+  std::cout << "[debug] Ctrl + B" << std::endl;
   break;
 
 case   3:
-  std::cout << "[debug] key code:   3" << std::endl;
+  std::cout << "[debug] Ctrl + C" << std::endl;
   break;
 
 case   4:
-  std::cout << "[debug] key code:   4" << std::endl;
+  std::cout << "[debug] Ctrl + D" << std::endl;
   break;
 
 case   5:
-  std::cout << "[debug] key code:   5" << std::endl;
+  std::cout << "[debug] Ctrl + E" << std::endl;
   break;
 
 case   6:
-  std::cout << "[debug] key code:   6" << std::endl;
+  std::cout << "[debug] Ctrl + F" << std::endl;
   break;
 
 case   7:
-  std::cout << "[debug] key code:   7" << std::endl;
+  std::cout << "[debug] Ctrl + G" << std::endl;
   break;
 
 case   8:
-  std::cout << "[debug] key code:   8" << std::endl;
+  std::cout << "[debug] Ctrl + H" << std::endl;
   break;
 
 case   9:
-  std::cout << "[debug] key code:   9" << std::endl;
+  std::cout << "[debug] Ctrl + I" << std::endl;
   break;
 
 case  10:
-  std::cout << "[debug] key code:  10" << std::endl;
+  std::cout << "[debug] Ctrl + J" << std::endl;
   break;
 
 case  11:
-  std::cout << "[debug] key code:  11" << std::endl;
+  std::cout << "[debug] Ctrl + K" << std::endl;
   break;
 
 case  12:
-  std::cout << "[debug] key code:  12" << std::endl;
+  std::cout << "[debug] Ctrl + L" << std::endl;
   break;
 
 case  13:
-  std::cout << "[debug] key code:  13" << std::endl;
+  std::cout << "[debug] Ctrl + M" << std::endl;
   break;
 
 case  14:
-  std::cout << "[debug] key code:  14" << std::endl;
+  std::cout << "[debug] Ctrl + N" << std::endl;
   break;
 
 case  15:
-  std::cout << "[debug] key code:  15" << std::endl;
+  std::cout << "[debug] Ctrl + O" << std::endl;
   break;
 
 case  16:
-  std::cout << "[debug] key code:  16" << std::endl;
+  std::cout << "[debug] Ctrl + P" << std::endl;
   break;
 
 case  17:
-  std::cout << "[debug] key code:  17" << std::endl;
+  std::cout << "[debug] Ctrl + Q" << std::endl;
   break;
 
 case  18:
-  std::cout << "[debug] key code:  18" << std::endl;
+  std::cout << "[debug] Ctrl + R" << std::endl;
   break;
 
 case  19:
-  std::cout << "[debug] key code:  19" << std::endl;
+  std::cout << "[debug] Ctrl + S" << std::endl;
   break;
 
 case  20:
-  std::cout << "[debug] key code:  20" << std::endl;
+  std::cout << "[debug] Ctrl + T" << std::endl;
   break;
 
 case  21:
-  std::cout << "[debug] key code:  21" << std::endl;
+  std::cout << "[debug] Ctrl + U" << std::endl;
   break;
 
 case  22:
-  std::cout << "[debug] key code:  22" << std::endl;
+  std::cout << "[debug] Ctrl + V" << std::endl;
   break;
 
 case  23:
-  std::cout << "[debug] key code:  23" << std::endl;
+  std::cout << "[debug] Ctrl + W" << std::endl;
   break;
 
 case  24:
-  std::cout << "[debug] key code:  24" << std::endl;
+  std::cout << "[debug] Ctrl + X" << std::endl;
   break;
 
 case  25:
-  std::cout << "[debug] key code:  25" << std::endl;
+  std::cout << "[debug] Ctrl + Y" << std::endl;
   break;
 
 case  26:
-  std::cout << "[debug] key code:  26" << std::endl;
+  std::cout << "[debug] Ctrl + Z" << std::endl;
   break;
 
 case  27:
-  std::cout << "[debug] key code:  27" << std::endl;
+  std::cout << "[debug] Escape, Ctrl + [" << std::endl;
   break;
 
 case  28:
-  std::cout << "[debug] key code:  28" << std::endl;
+  std::cout << "[debug] Ctrl + Space" << std::endl;
   break;
 
 case  29:
-  std::cout << "[debug] key code:  29" << std::endl;
+  std::cout << "[debug] Ctrl + ]" << std::endl;
   break;
 
 case  30:
-  std::cout << "[debug] key code:  30" << std::endl;
+  std::cout << "[debug] Ctrl + 6" << std::endl;
   break;
 
 case  31:
-  std::cout << "[debug] key code:  31" << std::endl;
+  std::cout << "[debug] Ctrl + -" << std::endl;
   break;
 
 case  32:
-  std::cout << "[debug] key code:  32" << std::endl;
+  std::cout << "[debug] Space" << std::endl;
   break;
 
 case  33:
-  std::cout << "[debug] key code:  33" << std::endl;
+  std::cout << "[debug] Ctrl + 1" << std::endl;
   break;
 
 case  34:
-  std::cout << "[debug] key code:  34" << std::endl;
+  std::cout << "[debug] Shift + Single Quote" << std::endl;
   break;
 
 case  35:
-  std::cout << "[debug] key code:  35" << std::endl;
+  std::cout << "[debug] Ctrl + 3" << std::endl;
   break;
 
 case  36:
-  std::cout << "[debug] key code:  36" << std::endl;
+  std::cout << "[debug] Ctrl + 4" << std::endl;
   break;
 
 case  37:
-  std::cout << "[debug] key code:  37" << std::endl;
+  std::cout << "[debug] Ctrl + 5" << std::endl;
   break;
 
 case  38:
-  std::cout << "[debug] key code:  38" << std::endl;
+  std::cout << "[debug] Ctrl + 7" << std::endl;
   break;
 
 case  39:
-  std::cout << "[debug] key code:  39" << std::endl;
+  std::cout << "[debug] Single Quote" << std::endl;
   break;
 
 case  40:
-  std::cout << "[debug] key code:  40" << std::endl;
+  std::cout << "[debug] Ctrl + 9" << std::endl;
   break;
 
 case  41:
-  std::cout << "[debug] key code:  41" << std::endl;
+  std::cout << "[debug] Ctrl + 0" << std::endl;
   break;
 
 case  42:
-  std::cout << "[debug] key code:  42" << std::endl;
+  std::cout << "[debug] Ctrl + 8" << std::endl;
   break;
 
 case  43:
-  std::cout << "[debug] key code:  43" << std::endl;
+  std::cout << "[debug] Shift + Equal" << std::endl;
   break;
 
 case  44:
-  std::cout << "[debug] key code:  44" << std::endl;
+  std::cout << "[debug] Comma" << std::endl;
   break;
 
 case  45:
-  std::cout << "[debug] key code:  45" << std::endl;
+  std::cout << "[debug] Hyphen" << std::endl;
   break;
 
 case  46:
-  std::cout << "[debug] key code:  46" << std::endl;
+  std::cout << "[debug] Dot, Shift + Delete (num keypad)" << std::endl;
   break;
 
 case  47:
-  std::cout << "[debug] key code:  47" << std::endl;
+  std::cout << "[debug] Slash" << std::endl;
   break;
 
 case  48:
-  std::cout << "[debug] key code:  48" << std::endl;
+  std::cout << "[debug] 0" << std::endl;
   break;
 
 case  49:
-  std::cout << "[debug] key code:  49" << std::endl;
+  std::cout << "[debug] 1" << std::endl;
   break;
 
 case  50:
-  std::cout << "[debug] key code:  50" << std::endl;
+  std::cout << "[debug] 2" << std::endl;
   break;
 
 case  51:
-  std::cout << "[debug] key code:  51" << std::endl;
+  std::cout << "[debug] 3" << std::endl;
   break;
 
 case  52:
-  std::cout << "[debug] key code:  52" << std::endl;
+  std::cout << "[debug] 4" << std::endl;
   break;
 
 case  53:
-  std::cout << "[debug] key code:  53" << std::endl;
+  std::cout << "[debug] 5" << std::endl;
   break;
 
 case  54:
-  std::cout << "[debug] key code:  54" << std::endl;
+  std::cout << "[debug] 6" << std::endl;
   break;
 
 case  55:
-  std::cout << "[debug] key code:  55" << std::endl;
+  std::cout << "[debug] 7" << std::endl;
   break;
 
 case  56:
-  std::cout << "[debug] key code:  56" << std::endl;
+  std::cout << "[debug] 8" << std::endl;
   break;
 
 case  57:
-  std::cout << "[debug] key code:  57" << std::endl;
+  std::cout << "[debug] 9" << std::endl;
   break;
 
 case  58:
-  std::cout << "[debug] key code:  58" << std::endl;
+  std::cout << "[debug] Shift + Semi Colon" << std::endl;
   break;
 
 case  59:
-  std::cout << "[debug] key code:  59" << std::endl;
+  std::cout << "[debug] Semi Colon" << std::endl;
   break;
 
 case  60:
-  std::cout << "[debug] key code:  60" << std::endl;
+  std::cout << "[debug] Shift + Comma" << std::endl;
   break;
 
 case  61:
-  std::cout << "[debug] key code:  61" << std::endl;
+  std::cout << "[debug] Equal" << std::endl;
   break;
 
 case  62:
-  std::cout << "[debug] key code:  62" << std::endl;
+  std::cout << "[debug] Shift + Dot" << std::endl;
   break;
 
 case  63:
-  std::cout << "[debug] key code:  63" << std::endl;
+  std::cout << "[debug] Shift + Slash" << std::endl;
   break;
 
 case  64:
-  std::cout << "[debug] key code:  64" << std::endl;
+  std::cout << "[debug] Ctrl + 2" << std::endl;
   break;
 
 case  65:
-  std::cout << "[debug] key code:  65" << std::endl;
+  std::cout << "[debug] Shift + A" << std::endl;
   break;
 
 case  66:
-  std::cout << "[debug] key code:  66" << std::endl;
+  std::cout << "[debug] Shift + B" << std::endl;
   break;
 
 case  67:
-  std::cout << "[debug] key code:  67" << std::endl;
+  std::cout << "[debug] Shift + C" << std::endl;
   break;
 
 case  68:
-  std::cout << "[debug] key code:  68" << std::endl;
+  std::cout << "[debug] Shift + D" << std::endl;
   break;
 
 case  69:
-  std::cout << "[debug] key code:  69" << std::endl;
+  std::cout << "[debug] Shift + E" << std::endl;
   break;
 
 case  70:
-  std::cout << "[debug] key code:  70" << std::endl;
+  std::cout << "[debug] Shift + F" << std::endl;
   break;
 
 case  71:
-  std::cout << "[debug] key code:  71" << std::endl;
+  std::cout << "[debug] Shift + G" << std::endl;
   break;
 
 case  72:
-  std::cout << "[debug] key code:  72" << std::endl;
+  std::cout << "[debug] Shift + H" << std::endl;
   break;
 
 case  73:
-  std::cout << "[debug] key code:  73" << std::endl;
+  std::cout << "[debug] Shift + I" << std::endl;
   break;
 
 case  74:
-  std::cout << "[debug] key code:  74" << std::endl;
+  std::cout << "[debug] Shift + J" << std::endl;
   break;
 
 case  75:
-  std::cout << "[debug] key code:  75" << std::endl;
+  std::cout << "[debug] Shift + K" << std::endl;
   break;
 
 case  76:
-  std::cout << "[debug] key code:  76" << std::endl;
+  std::cout << "[debug] Shift + L" << std::endl;
   break;
 
 case  77:
-  std::cout << "[debug] key code:  77" << std::endl;
+  std::cout << "[debug] Shift + M" << std::endl;
   break;
 
 case  78:
-  std::cout << "[debug] key code:  78" << std::endl;
+  std::cout << "[debug] Shift + N" << std::endl;
   break;
 
 case  79:
-  std::cout << "[debug] key code:  79" << std::endl;
+  std::cout << "[debug] Shift + O" << std::endl;
   break;
 
 case  80:
-  std::cout << "[debug] key code:  80" << std::endl;
+  std::cout << "[debug] Shift + P" << std::endl;
   break;
 
 case  81:
-  std::cout << "[debug] key code:  81" << std::endl;
+  std::cout << "[debug] Shift + Q" << std::endl;
   break;
 
 case  82:
-  std::cout << "[debug] key code:  82" << std::endl;
+  std::cout << "[debug] Shift + R" << std::endl;
   break;
 
 case  83:
-  std::cout << "[debug] key code:  83" << std::endl;
+  std::cout << "[debug] Shift + S" << std::endl;
   break;
 
 case  84:
-  std::cout << "[debug] key code:  84" << std::endl;
+  std::cout << "[debug] Shift + T" << std::endl;
   break;
 
 case  85:
-  std::cout << "[debug] key code:  85" << std::endl;
+  std::cout << "[debug] Shift + U" << std::endl;
   break;
 
 case  86:
-  std::cout << "[debug] key code:  86" << std::endl;
+  std::cout << "[debug] Shift + V" << std::endl;
   break;
 
 case  87:
-  std::cout << "[debug] key code:  87" << std::endl;
+  std::cout << "[debug] Shift + W" << std::endl;
   break;
 
 case  88:
-  std::cout << "[debug] key code:  88" << std::endl;
+  std::cout << "[debug] Shift + X" << std::endl;
   break;
 
 case  89:
-  std::cout << "[debug] key code:  89" << std::endl;
+  std::cout << "[debug] Shift + Y" << std::endl;
   break;
 
 case  90:
-  std::cout << "[debug] key code:  90" << std::endl;
+  std::cout << "[debug] Shift + Z" << std::endl;
   break;
 
 case  91:
-  std::cout << "[debug] key code:  91" << std::endl;
+  std::cout << "[debug] Left Bracket" << std::endl;
   break;
 
 case  92:
-  std::cout << "[debug] key code:  92" << std::endl;
+  std::cout << "[debug] Space" << std::endl;
   break;
 
 case  93:
-  std::cout << "[debug] key code:  93" << std::endl;
+  std::cout << "[debug] Right Bracket" << std::endl;
   break;
 
 case  94:
-  std::cout << "[debug] key code:  94" << std::endl;
+  std::cout << "[debug] Ctrl + 6" << std::endl;
   break;
 
 case  95:
-  std::cout << "[debug] key code:  95" << std::endl;
+  std::cout << "[debug] Ctrl + -" << std::endl;
   break;
 
 case  96:
-  std::cout << "[debug] key code:  96" << std::endl;
+  std::cout << "[debug] Back Quote" << std::endl;
   break;
 
 case  97:
-  std::cout << "[debug] key code:  97" << std::endl;
+  std::cout << "[debug] A" << std::endl;
   break;
 
 case  98:
-  std::cout << "[debug] key code:  98" << std::endl;
+  std::cout << "[debug] B" << std::endl;
   break;
 
 case  99:
-  std::cout << "[debug] key code:  99" << std::endl;
+  std::cout << "[debug] C" << std::endl;
   break;
 
 case 100:
-  std::cout << "[debug] key code: 100" << std::endl;
+  std::cout << "[debug] D" << std::endl;
   break;
 
 case 101:
-  std::cout << "[debug] key code: 101" << std::endl;
+  std::cout << "[debug] E" << std::endl;
   break;
 
 case 102:
-  std::cout << "[debug] key code: 102" << std::endl;
+  std::cout << "[debug] F" << std::endl;
   break;
 
 case 103:
-  std::cout << "[debug] key code: 103" << std::endl;
+  std::cout << "[debug] G" << std::endl;
   break;
 
 case 104:
-  std::cout << "[debug] key code: 104" << std::endl;
+  std::cout << "[debug] H" << std::endl;
   break;
 
 case 105:
-  std::cout << "[debug] key code: 105" << std::endl;
+  std::cout << "[debug] I" << std::endl;
   break;
 
 case 106:
-  std::cout << "[debug] key code: 106" << std::endl;
+  std::cout << "[debug] J" << std::endl;
   break;
 
 case 107:
-  std::cout << "[debug] key code: 107" << std::endl;
+  std::cout << "[debug] K" << std::endl;
   break;
 
 case 108:
-  std::cout << "[debug] key code: 108" << std::endl;
+  std::cout << "[debug] L" << std::endl;
   break;
 
 case 109:
-  std::cout << "[debug] key code: 109" << std::endl;
+  std::cout << "[debug] M" << std::endl;
   break;
 
 case 110:
-  std::cout << "[debug] key code: 110" << std::endl;
+  std::cout << "[debug] N" << std::endl;
   break;
 
 case 111:
-  std::cout << "[debug] key code: 111" << std::endl;
+  std::cout << "[debug] O" << std::endl;
   break;
 
 case 112:
-  std::cout << "[debug] key code: 112" << std::endl;
+  std::cout << "[debug] P" << std::endl;
   break;
 
 case 113:
-  std::cout << "[debug] key code: 113" << std::endl;
+  std::cout << "[debug] Q" << std::endl;
   break;
 
 case 114:
-  std::cout << "[debug] key code: 114" << std::endl;
+  std::cout << "[debug] R" << std::endl;
   break;
 
 case 115:
-  std::cout << "[debug] key code: 115" << std::endl;
+  std::cout << "[debug] S" << std::endl;
   break;
 
 case 116:
-  std::cout << "[debug] key code: 116" << std::endl;
+  std::cout << "[debug] T" << std::endl;
   break;
 
 case 117:
-  std::cout << "[debug] key code: 117" << std::endl;
+  std::cout << "[debug] U" << std::endl;
   break;
 
 case 118:
-  std::cout << "[debug] key code: 118" << std::endl;
+  std::cout << "[debug] V" << std::endl;
   break;
 
 case 119:
-  std::cout << "[debug] key code: 119" << std::endl;
+  std::cout << "[debug] W" << std::endl;
   break;
 
 case 120:
-  std::cout << "[debug] key code: 120" << std::endl;
+  std::cout << "[debug] X" << std::endl;
   break;
 
 case 121:
-  std::cout << "[debug] key code: 121" << std::endl;
+  std::cout << "[debug] Y" << std::endl;
   break;
 
 case 122:
-  std::cout << "[debug] key code: 122" << std::endl;
+  std::cout << "[debug] Z" << std::endl;
   break;
 
 case 123:
-  std::cout << "[debug] key code: 123" << std::endl;
+  std::cout << "[debug] Shift + Left Bracket" << std::endl;
   break;
 
 case 124:
-  std::cout << "[debug] key code: 124" << std::endl;
+  std::cout << "[debug] Shift + Space" << std::endl;
   break;
 
 case 125:
-  std::cout << "[debug] key code: 125" << std::endl;
+  std::cout << "[debug] Shift + Right Bracket" << std::endl;
   break;
 
 case 126:
-  std::cout << "[debug] key code: 126" << std::endl;
+  std::cout << "[debug] Ctrl + Back Quote" << std::endl;
   break;
 
 case 127:
-  std::cout << "[debug] key code: 127" << std::endl;
+  std::cout << "[debug] Backspace" << std::endl;
   break;
 
