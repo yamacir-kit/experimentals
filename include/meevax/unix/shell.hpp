91baf20d08550dbbfd105cdf50ef02454c44ff7e
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-28 23:13:22 +0900

    Cleanup

--- include/meevax/unix/shell.hpp
+++ include/meevax/unix/shell.hpp
@@ -44,17 +44,13 @@ private:
   static constexpr trial::static_concatenate<char_type> scat {};
 
   const std::vector<std::basic_string<char_type>> mode_message_ {
-    {"text"},
-    {"line"},
-    {"word"}
+    {"text"}, {"line"}, {"word"}
   };
 
   enum class semantic_parse_unit
     : typename decltype(mode_message_)::size_type
   {
-    text,
-    line,
-    word,
+    text, line, word,
   } parse_unit_;
 
   std::pair<std::size_t, std::size_t> cursor_;
@@ -62,18 +58,12 @@ private:
 public:
   explicit shell(int argc, char** argv)
     : text_buffer_ {},
-      // line_buffer_ {argv, argv + argc},
       line_buffer_ {},
       word_buffer_ {},
       char_buffer_ {},
       parse_unit_  {semantic_parse_unit::line},
       cursor_ {0, 0}
   {
-    // arguments_parse(line_buffer_);
-    //
-    // text_buffer_.push_back(line_buffer_);
-    // line_buffer_.clear();
-
     ::tcgetattr(STDIN_FILENO, &default_);
 
     struct termios ios {default_};
@@ -152,7 +142,8 @@ public:
   auto write() const
   {
     std::cout << "[semantic_parse_unit: "
-              << mode_message_[static_cast<typename std::underlying_type<decltype(parse_unit_)>::type>(parse_unit_)] << "]\n";
+              << mode_message_[static_cast<typename std::underlying_type<decltype(parse_unit_)>::type>(parse_unit_)]
+              << "]\n";
 
     for (const auto& line : text_buffer_)
     {
