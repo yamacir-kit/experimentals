277858be43c1b6fd1be116502997bb0e2ffb9760
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-12 20:16:31 +0900

    Replace cairo function to meevax visual manipulator

--- src/main.cpp
+++ src/main.cpp
@@ -46,10 +46,8 @@ int main(int argc, char** argv)
 
   while (true)
   {
-    vstream["subwin"] << meevax::map_raised;
-
-    cairo_set_source_rgba(vstream["subwin"], 0, 0, 0, 1.0);
-    cairo_paint(vstream["subwin"]);
+    vstream["subwin"] << meevax::map_raised
+                      << meevax::color(0, 0, 0) << meevax::paint;
 
     cairo_select_font_face(vstream["subwin"], "Ricty Diminished", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(vstream["subwin"], 20);
