4d4dc01652c8d77dac0a02d8ac36dd85452279ae
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-12 19:31:56 +0900

    Remove explicit keyword from constructor

--- include/meevax/visual/visual_context.hpp
+++ include/meevax/visual/visual_context.hpp
@@ -16,7 +16,7 @@ class visual_context
   std::unique_ptr<cairo_t, decltype(&cairo_destroy)> cairo_;
 
 public:
-  explicit visual_context(cairo_surface_t* surface)
+  visual_context(cairo_surface_t* surface)
     : cairo_ {cairo_create(surface), cairo_destroy}
   {}
 
