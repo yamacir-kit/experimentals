8e46c342755059d1b86370af5d43148acba9a476
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-12 18:38:38 +0900

    Add new class "visual_stream"

---
+++ include/meevax/visual/visual_stream.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_MEEVAX_VISUAL_VISUAL_STREAM_HPP_
+#define INCLUDED_MEEVAX_VISUAL_VISUAL_STREAM_HPP_
+
+
+namespace meevax {
+
+
+class visual_stream
+{};
+
+
+} // namespace meevax
+
+
+#endif
