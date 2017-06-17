0cbf66a767f1688ab2e76ec02d56db820cf2aeb3
YamaCIR-KIT <httperror@404-notfound.jp>
2017-06-17 20:41:07 +0900

    Remove unneeded debug codes

--- include/meevax/vstream/basic_vstream.hpp
+++ include/meevax/vstream/basic_vstream.hpp
@@ -92,25 +92,6 @@ auto meevax::basic_vstream<C>::create(const std::basic_string<C>& surface_name,
   };
 
   return typename decltype(surfaces_)::mapped_type {cairo_create(cairo_surface), cairo_destroy};
-
-  // surfaces_.emplace(
-  //   surface_name,
-  //   typename decltype(surfaces_)::mapped_type {cairo_create(cairo_surface), cairo_destroy}
-  // );
-  //
-  // XMapRaised(
-  //   display_.get(),
-  //   cairo_xlib_surface_get_drawable(cairo_get_target(surfaces_.at(surface_name).get()))
-  // );
-  //
-  // cairo_set_source_rgba(
-  //   surfaces_.at(surface_name).get(),
-  //   1.0, 0.0, 0.0, 1.0
-  // );
-  //
-  // cairo_paint(
-  //   surfaces_.at(surface_name).get()
-  // );
 }
 
 
