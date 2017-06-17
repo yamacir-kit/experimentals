539d30b351b7e87048ec96fee895977bf07fa699
YamaCIR-KIT <httperror@404-notfound.jp>
2017-06-17 17:50:31 +0900

    Add member function "create"
    visual_context 撲滅計画進行中
    代償として書いてる本人も把握ギリギリな魔界の誕生

--- include/meevax/vstream/basic_vstream.hpp
+++ include/meevax/vstream/basic_vstream.hpp
@@ -23,7 +23,11 @@ class basic_vstream
   > surfaces_;
 
 public:
-  basic_vstream(const std::string& name = {""});
+  basic_vstream(const std::basic_string<C>& name = {""});
+
+  // create();
+  auto create(const std::basic_string<C>& surface_name,
+              const std::basic_string<C>& parent_surface = {""});
 };
 
 } // namespace meevax
@@ -39,6 +43,52 @@ meevax::basic_vstream<C>::basic_vstream(const std::basic_string<C>& name)
     std::cerr << "[error] XOpenDisplay(3) - failed to open display " << name << std::endl;
     std::exit(EXIT_FAILURE);
   }
+}
+
+
+template <typename C>
+auto meevax::basic_vstream<C>::create(const std::basic_string<C>& surface_name,
+                                      const std::basic_string<C>& parent_surface)
+{
+  std::size_t width {320}; // TODO
+  std::size_t height {240}; // TODO
+  std::size_t border_line_width {1};
+  auto border_pixel_color {XBlackPixel(display_.get(), XDefaultScreen(display_.get()))};
+  auto background_pixel_color {XWhitePixel(display_.get(), XDefaultScreen(display_.get()))};
+
+  auto simple_window {
+    XCreateSimpleWindow(
+      display_.get(),
+      parent_surface.empty() ? XDefaultRootWindow(display_.get()) : cairo_xlib_surface_get_drawable(cairo_get_target(surfaces_.at(parent_surface).get())),
+      0, // position x form parent window
+      0, // position y form parent window
+      width,
+      height,
+      border_line_width,
+      border_pixel_color,
+      background_pixel_color
+    )
+  };
+
+  auto cairo_surface {
+    cairo_xlib_surface_create(
+      display_.get(),
+      simple_window,
+      XDefaultVisual(display_.get(), XDefaultScreen(display_.get())),
+      width,
+      height
+    )
+  };
+
+  surfaces_.emplace(
+    surface_name,
+    typename decltype(surfaces_)::mapped_type {cairo_create(cairo_surface), cairo_destroy}
+  );
+
+  XMapRaised(
+    display_.get(),
+    cairo_xlib_surface_get_drawable(cairo_get_target(surfaces_.at(surface_name).get()))
+  );
 }
 
 
