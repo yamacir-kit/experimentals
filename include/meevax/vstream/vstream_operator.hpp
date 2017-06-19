511b85506817eb7ea7b19ef8b4136548a1ab8774
YamaCIR-KIT <httperror@404-notfound.jp>
2017-06-19 18:03:37 +0900

    Cleanup

--- include/meevax/vstream/vstream_operator.hpp
+++ include/meevax/vstream/vstream_operator.hpp
@@ -8,8 +8,6 @@
 #include <X11/Xlib.h>
 #include <cairo/cairo-xlib.h>
 
-#include <meevax/vstream/vstream_manipulator.hpp>
-
 
 template <typename F>
 auto& operator<<(const std::unique_ptr<cairo_t, decltype(&cairo_destroy)>& cairo, F&& manip)
@@ -33,41 +31,6 @@ auto& operator<<(const std::unique_ptr<cairo_t, decltype(&cairo_destroy)>& cairo
   cairo_show_text(cairo.get(), text.c_str());
   return cairo;
 };
-
-
-// template <typename C = char>
-// auto& operator<<(const std::unique_ptr<cairo_t, decltype(&cairo_destroy)>& cairo,
-//                  std::vector<std::vector<std::basic_string<C>>>&& text)
-// {
-//   for (const auto& line : text)
-//   {
-//     for (const auto& word : line)
-//     {
-//       cairo_show_text(cairo.get(), (&word != &line.back() ? word + " " : word).c_str());
-//     }
-//     if (&line != &text.back()) { cairo << meevax::endl; }
-//   }
-//
-//   return cairo;
-// }
-//
-//
-// template <typename C = char>
-// auto& operator<<(const std::unique_ptr<cairo_t, decltype(&cairo_destroy)>& cairo,
-//                  const C* utf8)
-// {
-//   std::basic_stringstream<C> text {utf8};
-//
-//   std::vector<std::vector<std::basic_string<C>>> parsive {};
-//               std::vector<std::basic_string<C>>  line_buffer {};
-//
-//   for (std::basic_string<C> buffer {}; std::getline(text, buffer); parsive.push_back(line_buffer))
-//   {
-//     for (std::basic_stringstream<C> line {buffer}; std::getline(line, buffer, ' '); line_buffer.push_back(buffer));
-//   }
-//
-//   return cairo << std::move(parsive);
-// }
 
 
 #endif
