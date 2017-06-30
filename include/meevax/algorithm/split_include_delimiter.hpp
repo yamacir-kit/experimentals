c261c437913e3506c60aca6cb2c7b30068987790
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-01 06:57:14 +0900

    Update but remains bug
    バグは残っているけどとりあえず収拾がつかなくなる前にコミット
    
    それとデリミタを削除しない split が欲しくなったから boost の split はやめた

---
+++ include/meevax/algorithm/split_include_delimiter.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDED_MEEVAX_ALGORITHM_SPLIT_INCLUDE_DELIMITER_HPP_
+#define INCLUDED_MEEVAX_ALGORITHM_SPLIT_INCLUDE_DELIMITER_HPP_
+
+
+#include <algorithm>
+
+
+namespace meevax {
+  namespace algorithm {
+
+
+template <template <typename...> class V, typename S>
+void split_include_delimiter(V<S>& result, const S& target, S&& delims)
+{
+  for (auto iter {target.begin()}; iter != target.end(); )
+  {
+    auto last {std::find_first_of(iter, target.end(), delims.begin(), delims.end())};
+
+    // if (last == target.end())
+    // {
+    //   std::cout << "[debug] not found\n";
+    // }
+    // else
+    // {
+    //   std::cout << "[debug] found: " << std::distance(target.begin(), last) << std::endl;
+    // }
+
+    result.emplace_back(iter, last != target.end() ? ++last : last);
+    iter = last;
+  }
+}
+
+
+  } // namespace algorithm
+} // namespace meevax
+
+
+#endif
