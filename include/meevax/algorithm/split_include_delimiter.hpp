6479a48869530323fa8fc0a1bcd850dbca738ce0
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-01 10:12:26 +0900

    Cleanup

--- include/meevax/algorithm/split_include_delimiter.hpp
+++ include/meevax/algorithm/split_include_delimiter.hpp
@@ -9,6 +9,8 @@ namespace meevax {
   namespace algorithm {
 
 
+// TODO THIS MUST BE MORE EFFICIENTLY
+
 template <template <typename...> class V, typename S>
 void split_include_delimiter(V<S>& result, const S& target, S&& delims)
 {
@@ -16,34 +18,8 @@ void split_include_delimiter(V<S>& result, const S& target, S&& delims)
   {
     auto last {std::find_first_of(iter, target.end(), delims.begin(), delims.end())};
 
-#ifndef NDEBUG
-    std::cout << "[finder] ";
-    for (const auto& c : target)
-    {
-      if (&c == &*iter && &c == &*last) { std::cout << "\e[0m\e[1;33;42m"; }
-      else if (&c == &*iter) { std::cout << "\e[0m\e[1;33m"; }
-      else if (&c == &*last) { std::cout << "\e[0m\e[1;42m"; }
-      else { std::cout << "\e[0m"; }
-      std::cout << (c == '\e' ? "\\e" : c == '\n' ? "\\n" : std::string {c});
-    }
-    std::cout << "\e[0m\n";
-#endif
-
     result.emplace_back(iter != target.begin() ? iter - 1 : iter, last);
     iter = (last != target.end() ? ++last : last);
-
-#ifndef NDEBUG
-    std::cout << "[update] ";
-    for (const auto& c : target)
-    {
-      if (&c == &*iter && &c == &*last) { std::cout << "\e[0m\e[1;33;42m"; }
-      else if (&c == &*iter) { std::cout << "\e[0m\e[1;33m"; }
-      else if (&c == &*last) { std::cout << "\e[0m\e[1;42m"; }
-      else { std::cout << "\e[0m"; }
-      std::cout << (c == '\e' ? "\\e" : c == '\n' ? "\\n" : std::string {c});
-    }
-    std::cout << "\e[0m\n";
-#endif
   }
 
   for (const auto& c : delims)
