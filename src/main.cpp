d1d1855f58844d55e094457348fdfb1734667e1f
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-03 09:06:55 +0900

    Lipstick
    メイン関数の整頓
    俺は大分綺麗になったと思うが，マニピュレータだらけなのとどっちが綺麗かは好みかな

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,18 @@ int main(int argc, char** argv) try
 
   meevax::visual_node<char> vstream {display.get()};
 
-  vstream.raise() << "\e[28;28;28M";
-
-  vstream["title"].move((1280-640)/2, (720-200)/4).resize(640, 150).raise()
-    << "\e[28;28;28M";
+  vstream.raise();
+  vstream["title"].move(320, 130).resize(640, 150).raise();
+  vstream["debug"].move(600, 500).resize(320,  50).raise();
+  vstream["hello"].move( 50, 400).resize(500, 300).raise();
+  vstream["count"].resize(300, 60).raise();
 
   auto show_title = [&]()
   {
     vstream["title"]
-      << "\e[28;28;28M\e[208;208;208m"
       << meevax::face("Bitstream Charter")
       << meevax::size(90) << meevax::cursorhome
-      << "Meevax System"
+      << "\e[28;28;28M\e[208;208;208mMeevax System"
       << meevax::face("Sans")
       << meevax::size(35) << "\n" << meevax::size(25)
       << "Version " << project_version.data() << " Alpha "
@@ -39,29 +39,20 @@ int main(int argc, char** argv) try
       << "(" << cmake_build_type.data() << " Build)";
   };
 
-  vstream["debug"].move(600, (720-50)*3/4).resize(320, 50).raise()
-    << "\e[28;28;28M";
-
-  vstream["escseq"].move(50, 400).resize(500, 300).raise()
-    << "\e[28;28;28M";
-
   auto show_hello_world = [&]()
   {
-    vstream["escseq"]
-      << "\e[28;28;28M"
+    vstream["hello"]
       << meevax::face("Ricty Diminished")
       << meevax::size(12.0 + 1.5 * 10)
       << meevax::cursorhome
-      << "\e[175;135;255m#include \e[255;215;95m<iostream>\n\n"
+      << "\e[28;28;28M\e[175;135;255m#include \e[255;215;95m<iostream>\n\n"
       << "\e[135;215;95mint \e[208;208;208mmain("
-      << "\e[135;215;95mint \e[208;208;208margc, \e[135;215;95mchar\e[208;208;208m** argv)\n"
+      << "\e[135;215;95mint \e[208;208;208margc, "
+      << "\e[135;215;95mchar\e[208;208;208m** argv)\n"
       << "{\n  std::cout << \e[255;215;95m\"hello, world!\e[255;95;00m"
       << "\e[255;215;95m\"\e[208;208;208m;\n\n"
       << "  \e[255;00;135mreturn \e[00;215;215m0\e[208;208;208m;\n}";
   };
-
-  vstream["serial"].resize(300, 60).raise()
-    << "\e[28;28;28M";
 
   while (true)
   {
@@ -74,7 +65,7 @@ int main(int argc, char** argv) try
       {
         vstream.resize(0, 0) << "\e[28;28;28M";
 
-        vstream["serial"]
+        vstream["count"]
           << meevax::face("Ricty Diminished") << meevax::size(12.0 + 1.5 * 10)
           << meevax::cursorhome
           << "\e[28;28;28M\e[208;208;208mserial: " << std::to_string(event.xexpose.serial);
