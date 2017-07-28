1ccf1428cc63a17a68dc871014ffcf7c821691e4
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-28 16:04:53 +0900

    Add template class "meevax::xcb::iterator"

---
+++ include/meevax/xcb/iterator.hpp
@@ -0,0 +1,49 @@
+#ifndef INCLUDED_MEEVAX_XCB_ITERATOR_HPP_
+#define INCLUDED_MEEVAX_XCB_ITERATOR_HPP_
+
+
+#include <iterator>
+#include <type_traits>
+
+#include <xcb/xcb.h>
+
+
+namespace meevax::xcb {
+
+
+template <typename T, typename XcbIterator, typename NextFunctor, NextFunctor Next>
+class iterator
+  : public std::iterator<std::forward_iterator_tag, T>,
+    public XcbIterator
+{
+public:
+  explicit iterator(const XcbIterator& iter)
+    : XcbIterator {iter}
+  {}
+
+  explicit iterator(const xcb_generic_iterator& iter)
+    : XcbIterator {reinterpret_cast<const XcbIterator&>(iter)}
+  {}
+
+public:
+  template <typename U>
+  auto operator==(const U& rhs)
+    -> typename std::enable_if<std::is_convertible<U,XcbIterator>::value, bool>::type
+  {
+    return (*this).data == rhs.data;
+  }
+
+  template <typename U>
+  auto operator!=(const U& rhs)
+    -> typename std::enable_if<std::is_convertible<U,XcbIterator>::value, bool>::type
+  {
+    return (*this).data != rhs.data;
+  }
+};
+
+
+} // namespace meevax::xcb
+
+
+#endif
+
