8758639aacec05d78ad8cb41b662598ce90ef6c7
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-30 06:05:44 +0900

    Update constructor

--- include/meevax/xcb/context.hpp
+++ include/meevax/xcb/context.hpp
@@ -17,39 +17,40 @@ namespace meevax::xcb {
 class window
 {
   const std::shared_ptr<xcb_connection_t> connection_;
+  meevax::xcb::accessor<xcb_setup_t, xcb_screen_t> screen_;
 
 public:
   const xcb_window_t id;
 
   explicit window(const std::shared_ptr<xcb_connection_t>& connection, xcb_window_t parent_id)
     : connection_ {connection},
+      screen_ {xcb_get_setup(connection_.get())},
       id {xcb_generate_id(connection_.get())}
   {
-    meevax::xcb::accessor<xcb_setup_t, xcb_screen_t> screen_access {
-      xcb_get_setup(connection_.get())
-    };
-
-    xcb_create_window(
-      connection_.get(), XCB_COPY_FROM_PARENT, id, parent_id,
-      0, 0, 1, 1, 1,
-      XCB_WINDOW_CLASS_INPUT_OUTPUT, screen_access.begin()->root_visual, 0, nullptr
-    );
+    create(parent_id, 0, 0);
   }
 
-  auto create_window(xcb_window_t parent_id,
-                     std::uint16_t width, std::uint16_t height, std::uint16_t border_width,
-                     xcb_visualid_t visual)
+  explicit window(const std::shared_ptr<xcb_connection_t>& connection)
+    : connection_ {connection},
+      screen_ {xcb_get_setup(connection_.get())},
+      id {xcb_generate_id(connection_.get())}
   {
-    return xcb_create_window(
-      connection_.get(), XCB_COPY_FROM_PARENT, id, parent_id, 0, 0,
-      std::max<std::uint16_t>(1, width), std::max<std::uint16_t>(1, height), border_width,
-      XCB_WINDOW_CLASS_INPUT_OUTPUT, visual, 0, nullptr
-    );
+    create(screen_.root, 0, 0);
   }
 
   const auto& connection() const noexcept
   {
     return connection_;
+  }
+
+private:
+  auto create(xcb_window_t parent_id, std::uint16_t width, std::uint16_t height)
+  {
+    return xcb_create_window(
+      connection_.get(), XCB_COPY_FROM_PARENT, id, parent_id, 0, 0,
+      std::max<std::uint16_t>(1, width), std::max<std::uint16_t>(1, height), 0
+      XCB_WINDOW_CLASS_INPUT_OUTPUT, screen_.begin()->root_visual, 0, nullptr
+    );
   }
 };
 
