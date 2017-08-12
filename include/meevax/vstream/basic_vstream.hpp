a58fb11c68f6cb3c179495d101d8a9e7f9120de4
YamaCIR-KIT <httperror@404-notfound.jp>
2017-08-12 23:44:53 +0900

    Remove basic_xlib_vstream [uncompilable]

--- include/meevax/vstream/basic_vstream.hpp
+++ include/meevax/vstream/basic_vstream.hpp
@@ -7,9 +7,6 @@
 #include <utility>
 #include <vector>
 
-#include <X11/Xlib.h>
-#include <cairo/cairo-xlib.h>
-
 #include <xcb/xcb.h>
 #include <cairo/cairo-xcb.h>
 
@@ -17,64 +14,6 @@
 
 
 namespace meevax {
-
-
-template <typename C>
-class basic_xlib_vstream
-  : public meevax::cairo::xlib::context
-{
-public:
-  explicit basic_xlib_vstream(Display* display)
-    : meevax::cairo::xlib::context {display, XDefaultRootWindow(display)}
-  {}
-
-  explicit basic_xlib_vstream(const meevax::basic_xlib_vstream<C>& vstream)
-    : meevax::cairo::xlib::context {static_cast<Display*>(vstream), static_cast<Window>(vstream)}
-  {}
-
-public:
-  auto& map() const noexcept
-  {
-    XMapWindow(static_cast<Display*>(*this), static_cast<Window>(*this));
-    return *this;
-  }
-
-  auto& unmap() const noexcept
-  {
-    XUnmapWindow(static_cast<Display*>(*this), static_cast<Window>(*this));
-    return *this;
-  }
-
-  auto& raise() const noexcept
-  {
-    XMapRaised(static_cast<Display*>(*this), static_cast<Window>(*this));
-    return *this;
-  }
-
-  auto& move(std::size_t x, std::size_t y) const noexcept
-  {
-    XMoveWindow(static_cast<Display*>(*this), static_cast<Window>(*this), x, y);
-    return *this;
-  }
-
-  auto& resize(std::size_t width, std::size_t height) const noexcept
-  {
-    static XWindowAttributes a;
-    XGetWindowAttributes(static_cast<Display*>(*this), static_cast<Window>(*this), &a);
-
-    XResizeWindow(static_cast<Display*>(*this), static_cast<Window>(*this), width != 0 ? width : a.width, height != 0 ? height : a.height);
-    cairo_xlib_surface_set_size(static_cast<cairo_surface_t*>(*this), a.width, a.height);
-
-    return *this;
-  }
-
-  auto& event()
-  {
-    static XEvent event_;
-    XNextEvent(static_cast<Display*>(*this), &event_);
-    return event_;
-  }
-};
 
 
 template <typename C>
@@ -109,10 +48,6 @@ public:
     cairo_xcb_surface_set_size((*this).get(), width, height);
   }
 };
-
-
-template <typename C>
-using basic_vstream = basic_xlib_vstream<C>;
 
 
 } // namespace meevax
