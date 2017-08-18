3dc5d65a105c0cacd93093dc4a82c2fd1e4e7137
YamaCIR-KIT <httperror@404-notfound.jp>
2017-08-19 00:16:10 +0900

    Add metafunction "has_value_type"

--- include/meevax/type_traits/has_function_call_operator.hpp
+++ include/meevax/type_traits/has_function_call_operator.hpp
@@ -9,32 +9,28 @@
 namespace meevax {
 
 
-namespace has_function_call_operator_
+struct has_function_call_operator_
 {
   template <typename Functor, typename... Ts>
-  constexpr auto check(Functor&& functor, Ts&&... args)
+  static constexpr auto check(Functor&& functor, Ts&&... args) noexcept
     -> decltype(functor(std::forward<Ts>(args)...), std::true_type {})
   {
     return {};
   }
 
   template <typename Functor>
-  constexpr auto check(...)
-    -> std::false_type
+  static constexpr decltype(auto) check(...) noexcept
   {
-    return {};
+    return std::false_type {};
   }
-} // namespace has_function_call_operator_
+};
 
 
 template <typename T, typename... Ts>
 using has_function_call_operator
-  = decltype(
-      has_function_call_operator_::check<T>(
-        std::forward<T>(std::declval<T>()),
-        std::forward<Ts>(std::declval<Ts>())...
-      )
-    );
+  = decltype(has_function_call_operator_::check<T>(
+      std::forward<T>(std::declval<T>()), std::forward<Ts>(std::declval<Ts>())...
+    ));
 
 
 } // namespace meevax
