f4001b2b857a11651b66cd1db159cb6bfc515465
YamaCIR-KIT <httperror@404-notfound.jp>
2017-08-23 19:11:33 +0900

    Update to parse ascii control characters

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <system_error>
 
@@ -61,6 +62,50 @@ int main(int argc, char** argv) try
     case XCB_KEY_PRESS:
       [&]() -> void
       {
+        std::unordered_map<std::string, std::pair<char, std::string>> ascii_control_characters {
+          {"^@", {0x00, "NUL: Null"}},
+          {"^A", {0x01, "SOH: Start of Heading"}},
+          {"^B", {0x02, "STX: Start of Text"}},
+          {"^C", {0x03, "ETX: End of Text"}},
+          {"^D", {0x04, "EOT: End of Transmission"}},
+          {"^E", {0x05, "ENQ: Enquiry"}},
+          {"^F", {0x06, "ACK: Acknowledgement"}},
+          {"^G", {0x07, "BEL: Bell"}},
+          {"^H", {0x08, "BS: Backspace"}},
+          {"^I", {0x09, "HT: Hrizontal Tab"}},
+          {"^J", {0x0A, "LF: Line Feed"}},
+          {"^K", {0x0B, "VT: Vertical Tab"}},
+          {"^L", {0x0C, "FF: Form Feed"}},
+          {"^M", {0x0D, "CR: Carriage Return"}},
+          {"^N", {0x0E, "SO: Shift Out"}},
+          {"^O", {0x0F, "SI: Shift In"}},
+          {"^P", {0x10, "DLE: Data Link Escape"}},
+          {"^Q", {0x11, "DC1: Device Control 1"}},
+          {"^R", {0x12, "DC2: Device Control 2"}},
+          {"^S", {0x13, "DC3: Device Control 3"}},
+          {"^T", {0x14, "DC4: Device Control 4"}},
+          {"^U", {0x15, "NAK: Negative Acknowledgement"}},
+          {"^V", {0x16, "SYN: Synchronous Idle"}},
+          {"^W", {0x17, "ETB: End of Transmission Block"}},
+          {"^X", {0x18, "CAN: Cancel"}},
+          {"^Y", {0x19, "EM: End of Medium"}},
+          {"^Z", {0x1A, "SUB: Substitute"}},
+          {"^[", {0x1B, "ESC: Escape"}},
+          {"^\\",{0x1C, "FS: File Separator"}},
+          {"^]", {0x1D, "GS: Group Separator"}},
+          {"^^", {0x1E, "RS: Record Separator"}},
+          {"^_", {0x1F, "US: Unit Separator"}},
+          {"^?", {0x7F, "DEL: Delete"}},
+        };
+
+        ascii_control_characters["^2"] = ascii_control_characters["^@"];
+        ascii_control_characters["^3"] = ascii_control_characters["^["];
+        ascii_control_characters["^4"] = ascii_control_characters["^\\"];
+        ascii_control_characters["^5"] = ascii_control_characters["^]"];
+        ascii_control_characters["^6"] = ascii_control_characters["^^"];
+        ascii_control_characters["^7"] = ascii_control_characters["^_"];
+        ascii_control_characters["^8"] = ascii_control_characters["^?"];
+
         auto keysym {xcb_key_press_lookup_keysym(
           std::unique_ptr<xcb_key_symbols_t, decltype(&xcb_key_symbols_free)> {
             xcb_key_symbols_alloc(connection.get()), xcb_key_symbols_free
@@ -69,17 +114,29 @@ int main(int argc, char** argv) try
           reinterpret_cast<xcb_key_press_event_t*>(generic_event.get())->state & ~XCB_MOD_MASK_CONTROL
         )};
 
+        if (xcb_is_modifier_key(keysym))
+        {
+          return;
+        }
+
         switch (auto modmask {reinterpret_cast<xcb_key_press_event_t*>(generic_event.get())->state})
         {
         case XCB_MOD_MASK_CONTROL:
         case XCB_MOD_MASK_CONTROL | XCB_MOD_MASK_SHIFT:
-          std::cout << "^" << static_cast<char>(keysym) << std::flush;
+          {
+            std::string caret {'^', static_cast<char>(std::toupper(keysym))};
+            if (ascii_control_characters.find(caret) != std::end(ascii_control_characters))
+            {
+              std::cout << "[debug] " << caret << ", ("
+                        << ascii_control_characters.at(caret).second << ")" << std::endl;
+            }
+          }
           break;
 
         default:
-          if (31 < keysym && keysym < 127)
+          if (0x1F < keysym && keysym < 0x7F)
           {
-            std::cout << static_cast<char>(keysym) << std::flush;
+            std::cout << "[debug] " << static_cast<char>(keysym) << std::endl;
           }
           break;
         }
