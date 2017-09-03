af19ee642da1163059bc2e23267b7bcb27403051
YamaCIR-KIT <httperror@404-notfound.jp>
2017-09-03 19:55:33 +0900

    Add header "has_type.hpp"

---
+++ include/meevax/type_traits/has_type.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDED_MEEVAX_TYPE_TRAITS_HAS_TYPE_HPP
+#define INCLUDED_MEEVAX_TYPE_TRAITS_HAS_TYPE_HPP
+
+
+#include <type_traits>
+#include <utility>
+
+
+#define implement_has_type(token) \
+struct has_##token##_ \
+{ \
+  template <typename T, typename = typename T::token> \
+  static constexpr decltype(auto) check(T&) noexcept \
+  { return std::true_type {}; } \
+\
+  template <typename T> \
+  static constexpr decltype(auto) check(...) noexcept \
+  { return std::false_type {}; } \
+}; \
+\
+template <typename T> \
+class has_##token \
+  : public decltype(has_##token##_::check<T>(std::declval<T>())) \
+{};
+
+
+inplement_has_type(allocator_type);
+inplement_has_type(iterator);
+inplement_has_type(const_iterator);
+inplement_has_type(value_type);
+inplement_has_type(size_type);
+
+
+#undef inplement_has_type
+
+
+#endif
+
