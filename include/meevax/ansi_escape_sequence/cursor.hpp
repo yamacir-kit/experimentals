3cba2672ad5c791e52039cb1462060389e79b6b4
yamacir-kit <httperror@404-notfound.jp>
2017-11-13 01:22:27 +0900

    Move counted_newline to header

--- include/meevax/ansi_escape_sequence/cursor.hpp
+++ include/meevax/ansi_escape_sequence/cursor.hpp
@@ -2,6 +2,9 @@
 #define INCLUDED_MEEVAX_ANSI_ESCAPE_SEQUENCE_CURSOR_HPP
 
 
+#include <iostream>
+#include <string>
+
 #include <meevax/string/static_concat.hpp>
 
 
@@ -17,7 +20,50 @@ static constexpr auto newline {meevax::string::static_concat("\n")};
 #endif
 
 
+class counted_newline_
+{
+  static signed int count_;
+
+public:
+  explicit counted_newline_() = default;
+
+  auto countup() noexcept
+  {
+    return (++count_, newline);
+  }
+
+  auto restore() noexcept // XXX carriage return はコイツの仕事ではない
+  {
+    const std::string cursor_up {
+      std::string {"\e["} + std::to_string(count_) + std::string {"A"}
+    };
+
+    count_ = 0;
+    return cursor_up + "\r\e[K";
+  }
+} counted_newline;
+
+
+signed int counted_newline_::count_ {0};
+
+
 } // namespace meevax::ansi_escape_sequence::cursor
+
+
+namespace {
+
+
+using namespace meevax::ansi_escape_sequence::cursor;
+
+
+template <typename Char>
+inline decltype(auto) operator<<(std::basic_ostream<Char>& lhs, counted_newline_& rhs)
+{
+  return lhs << rhs.countup();
+}
+
+
+} // namespace
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -19,47 +19,6 @@
 #include <meevax/posix/termios.hpp>
 
 
-namespace meevax::ansi_escape_sequence::cursor {
-
-
-class counted_newline_
-{
-  static signed int count_;
-
-public:
-  explicit counted_newline_() = default;
-
-  auto countup() noexcept
-  {
-    return (++count_, newline);
-  }
-
-  auto restore() noexcept // XXX carriage return はコイツの仕事ではない
-  {
-    const std::string cursor_up {
-      std::string {"\e["} + std::to_string(count_) + std::string {"A"}
-    };
-
-    count_ = 0;
-    return cursor_up + "\r\e[K";
-  }
-} counted_newline;
-
-
-signed int counted_newline_::count_ {0};
-
-
-} // namespace meevax::ansi_escape_sequence::cursor
-
-
-template <typename Char>
-inline decltype(auto) operator<<(std::basic_ostream<Char>& lhs,
-                                 meevax::ansi_escape_sequence::cursor::counted_newline_& rhs)
-{
-  return lhs << rhs.countup();
-}
-
-
 class window_size
   : public ::winsize
 {
