2e8a3914055b9af4902d11f688e8d92af1a35079
yamacir-kit <httperror@404-notfound.jp>
2017-12-18 18:41:07 +0900

    Fix bug of module "semantic/read"
    なんでこんなしょうもないコードを動かすのにアホみたいに時間がかかったのか
    これがわからない

--- include/meevax/semantic/read.hpp
+++ include/meevax/semantic/read.hpp
@@ -3,7 +3,7 @@
 
 
 static_assert(
-  201103L <= __cplusplus,
+  201703L <= __cplusplus,
   "module \"meevax/semantic/read.hpp\" will be available in C++11 or later."
 );
 
@@ -29,38 +29,25 @@ namespace meevax::semantic {
 #endif
 
 
-// template <typename...>
-// template <typename CharType, typename SemanticRange>
-template <typename CharType, typename SemanticRange, typename...>
-class read
-{
-  static_assert(false);
-};
+template <typename CharType, typename SemanticRange, typename = void>
+class read_;
 
 
-template <typename CharType, typename SemanticRange>
-class read<CharType, SemanticRange,
-           typename std::enable_if<
-                      meevax::concepts::is_char_type<CharType>::value
-                    >::type,
-           typename std::enable_if<
-                      std::is_same<
-                        CharType, SemanticRange
-                      >::value
-                    >::type>
+template <typename CharType>
+class read_<CharType, CharType,
+            typename std::enable_if<
+                       meevax::concepts::is_char_type<CharType>::value
+                     >::type>
 {
   using char_type = CharType;
 
 public:
-  template <typename... Ts>
-  auto operator()(Ts&&... args) -> char_type;
-
-  decltype(auto) operator()() noexcept
+  decltype(auto) operator()()
   {
     return static_cast<char_type>(std::getchar());
   }
 
-  decltype(auto) operator()(int fd) noexcept(false)
+  decltype(auto) operator()(int fd)
   {
     char_type buffer {};
     return (::read(fd, &buffer, sizeof(char_type)) < 0) ? buffer : throw std::system_error {errno, std::system_category()};
@@ -73,77 +60,28 @@ public:
 };
 
 
-template <typename CharType, typename SemanticRange>
-class read<CharType, SemanticRange,
-           typename std::enable_if<
-                      meevax::concepts::is_char_type<CharType>::value
-                    >::type,
-           typename std::enable_if<
-                      meevax::concepts::is_standard_container<SemanticRange>::value
-                    >::type>
+template <typename CharType>
+class read_<CharType, std::basic_string<CharType>,
+            typename std::enable_if<
+                       meevax::concepts::is_char_type<CharType>::value
+                     >::type>
 {
   using char_type = CharType;
 
 public:
-  template <typename... Ts>
-  auto operator()(Ts&&... args) -> SemanticRange;
-
-  template <typename = typename std::enable_if<
-                                  std::is_same<
-                                    std::basic_string<CharType>, SemanticRange
-                                  >::value
-                                >::type,
-            typename...>
   decltype(auto) operator()(std::basic_istream<char_type>& istream = std::cin,
                             char_type delimiter = '\n')
   {
-    SemanticRange buffer {};
+    std::basic_string<char_type> buffer {};
     std::getline(istream, buffer, delimiter);
 
     return std::move(buffer);
   }
-
-  // template <typename = typename std::enable_if<
-  //                                 not std::is_same<
-  //                                   std::basic_string<CharType>, SemanticRange
-  //                                 >::value
-  //                               >::type,
-  //           typename = typename std::enable_if<
-  //                                 std::is_constructible<
-  //                                   SemanticRange, std::basic_string<CharType>
-  //                                 >::value
-  //                               >::type>
-  // decltype(auto) operator()(std::basic_istream<char_type>& istream = std::cin,
-  //                           char_type delimiter = '\n')
-  // {
-  //   std::basic_string<CharType> buffer {};
-  //   std::getline(istream, buffer, delimiter);
-  //
-  //   return std::move(SemanticRange {buffer});
-  // }
-
-  template <typename = typename std::enable_if<
-                                  not std::is_same<
-                                    std::basic_string<CharType>, SemanticRange
-                                  >::value
-                                >::type>
-            // typename = typename std::enable_if<
-            //                       not std::is_constructible<
-            //                         SemanticRange, std::basic_string<CharType>
-            //                       >::value
-            //                     >::type>
-  decltype(auto) operator()(std::basic_istream<char_type>& istream = std::cin,
-                            char_type delimiter = '\n')
-  {
-    std::basic_string<CharType> buffer {};
-    std::getline(istream, buffer, delimiter);
-
-    SemanticRange result {};
-    std::copy(std::begin(buffer), std::end(buffer), std::back_inserter(result));
-
-    return std::move(result);
-  }
 };
+
+
+template <typename... Ts>
+read_<Ts...> read {};
 
 
 #if __cplusplus < 201703L
