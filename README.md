fb2b5bc50c91510b0e4a99c958b847063a7c57bc
yamacir-kit <httperror@404-notfound.jp>
2017-12-20 01:06:05 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-## meevax
-
-[![Build Status](https://travis-ci.org/yamacir-kit/meevax.svg?branch=master)](https://travis-ci.org/yamacir-kit/meevax)
+# meevax [![Build Status](https://travis-ci.org/yamacir-kit/meevax.svg?branch=master)](https://travis-ci.org/yamacir-kit/meevax)
 
 version 0.3.1 in development
 
 
-### dependency
+## Dependency
 
 - Boost C++ Libraries 1.58.0
 - Cairo Vector Graphics Library 1.14.6
--- configure/README.md.cmake
+++ configure/README.md.cmake
@@ -1,11 +1,9 @@
-## ${PROJECT_NAME}
-
-[![Build Status](https://travis-ci.org/yamacir-kit/meevax.svg?branch=master)](https://travis-ci.org/yamacir-kit/meevax)
+# ${PROJECT_NAME} [![Build Status](https://travis-ci.org/yamacir-kit/meevax.svg?branch=master)](https://travis-ci.org/yamacir-kit/meevax)
 
 version ${PROJECT_VERSION} in development
 
 
-### dependency
+## Dependency
 
 - Boost C++ Libraries ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}
 - Cairo Vector Graphics Library 1.14.6
