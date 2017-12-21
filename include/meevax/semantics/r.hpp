f6e19b01fcd5d8deba0f42397b0f228d775d3815
yamacir-kit <httperror@404-notfound.jp>
2017-12-21 20:47:46 +0900

    Update documentation

--- include/meevax/semantics/r.hpp
+++ include/meevax/semantics/r.hpp
@@ -2,6 +2,13 @@
 #define INCLUDED_MEEVAX_SEMANTICS_R_HPP
 
 
+/**
+* @file r.hpp
+*
+* Semantics for a character \b `r` implementation.
+*/
+
+
 #include <algorithm>
 #include <iostream>
 #include <iterator>
@@ -24,7 +31,9 @@ namespace meevax::semantics {
 
 
 /**
-* General template for character \b r implementation.
+* @class r_ r.hpp <meevax/semantics/r.hpp>
+*
+* General template for a character \b `r` implementation.
 * This class must not be instantiated.
 */
 template <
@@ -32,12 +41,14 @@ template <
   typename SemanticScope, typename = void
 #else
   typename SemanticScope
-#endif
+#endif // #ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
 >
 class r_;
 
 
 /**
+* @class r_<CharType> r.hpp <meevax/semantics/r.hpp>
+*
 * template specialization for CharType.
 *
 * @tparam CharType this type requires following concepts
@@ -54,94 +65,129 @@ class r_<
            >::type
 #else
   CharType
-#endif
+#endif // #ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
 >
 {
-public:
-  /// type definition for metafunction.
+public: // types
+  /**
+  * type definition for metafunction.
+  */
   using char_type = CharType;
 
-  /// type definition for metafunction.
+  /**
+  * type definition for metafunction.
+  */
   using value_type = typename std::basic_string<CharType>::value_type;
 
-private:
-  /// internal buffer.
+private: // attribs
+  /**
+  * static internal buffer.
+  */
   static inline value_type buffer_;
 
 public:
   /**
-   * read a character from file descriptor.
-   *
-   * @param[in] fd
-   * @returns a character read in.
-   */
-  decltype(auto) operator()(int fd = STDIN_FILENO)
+  * read a character into internal buffer from file descriptor.
+  *
+  * @param fd
+  * @returns const reference to internal buffer.
+  */
+  const decltype(auto) operator()(int fd = STDIN_FILENO)
   {
     return 0 < ::read(fd, &buffer_, sizeof buffer_) ? buffer_ : throw std::system_error {errno, std::system_category()};
   }
 
   /**
-   * read a character from standard input stream.
-   *
-   * @param[in] istream
-   * @returns a character read in.
-   */
-  decltype(auto) operator()(std::basic_istream<char_type>& istream)
+  * read a character into internal buffer from standard input stream.
+  *
+  * @param istream
+  * @returns const reference to internal buffer.
+  */
+  const decltype(auto) operator()(std::basic_istream<char_type>& istream)
   {
     return static_cast<char_type>(istream.get());
   }
 };
 
 
-template <typename CharType>
-class r_<std::basic_string<CharType>,
-         typename std::enable_if<
-                    meevax::concepts::is_char_type<CharType>::value
-                  >::type>
+/**
+* @class r_<StandardContainer> r.hpp <meevax/semantics/r.hpp>
+*
+* template specialization for StandardContainer.
+*
+* @tparam StandardContainer this type requires following concepts
+* @code
+*   meevax::concepts::is_standard_container<StandardContainer>::value == true
+* @endcode
+*/
+template <typename StandardContainer>
+class r_<
+#ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
+  StandardContainer,
+  typename std::enable_if<
+             meevax::concepts::is_standard_container<StandardContainer>::value
+           >::type
+#else
+  StandardContainer
+#endif // #ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
+>
+  : public StandardContainer
 {
-  using char_type = CharType;
+public: // types
+  /**
+  * type definition for metafunction.
+  */
+  using value_type = StandardContainer;
+
+private: // attribs
+  /**
+  * static internal buffer.
+  */
+  static inline value_type buffer_;
 
 public:
-  decltype(auto) operator()(std::basic_istream<char_type>& istream = std::cin, char_type delimiter = '\n')
+  /**
+  * read a line into internal buffer from standard input stream.
+  * this function enable if the template parameter StandardContainer is equivalent
+  * to std::basic_string<CharType>.
+  *
+  * @tparam CharType this type requires following concepts
+  * @code
+  *   meevax::concepts::is_char_type<CharType>::value == true
+  * @endcode
+  * @code
+  *   std::is_same<std::basic_string<CharType>, StandardContainer>::value == true
+  * @endcode
+  *
+  * @param istream
+  * @returns const reference to internal buffer.
+  */
+  template <
+#ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
+    typename CharType = typename StandardContainer::value_type,
+    typename = typename std::enable_if<
+                          meevax::concepts::is_char_type<CharType>::value
+                        >::type,
+    typename = typename std::enable_if<
+                          std::is_same<
+                            std::basic_string<CharType>,
+                            StandardContainer
+                          >::value
+                        >::type
+#else
+    typename CharType = typename StandardContainer::value_type
+#endif // #ifndef DOXYGEN_TEMPLATE_SFINAE_CONCEALER
+  >
+  const decltype(auto) operator()(std::basic_istream<CharType>& istream = std::cin)
   {
-    std::basic_string<char_type> buffer {};
-    std::getline(istream, buffer, delimiter);
-
-    return std::move(buffer);
+    std::getline(istream, buffer_);
+    return buffer_;
   }
 };
 
 
-// template <typename StandardContainer>
-// class r_<StandardContainer,
-//          typename std::enable_if<
-//                     meevax::concepts::is_standard_container<StandardContainer>::value
-//                   >::type>
-// {
-// public:
-//   template <typename CharType,
-//             typename = typename std::enable_if<
-//                                   meevax::concepts::is_char_type<CharType>::value
-//                                 >::type>
-//   decltype(auto) operator()(std::basic_istream<CharType>& istream = std::cin)
-//   {
-//     StandardContainer buffer {};
-//
-//     for ()
-//     {
-//       buffer.emplace(
-//         std::end(buffer),
-//         meevax::semantics::r<typename StandardContainer::value_type>(
-//           std::forward<decltype(istream)>(istream)
-//         )
-//       );
-//     }
-//   }
-// };
-
-
 template <typename... Ts>
-meevax::semantics::r_<Ts...> r;
+static meevax::semantics::r_<Ts...> r;
 
 
 } // namespace meevax::semantics
